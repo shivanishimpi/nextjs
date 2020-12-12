e7cf3f6afa64ba55e584c7ccc4d954ae71f0941c
shivanishimpi <shivanishimpi9@gmail.com>
2020-12-12 22:46:40 +0530

    📋posts-jsonAPI-getInitialProps

--- components/linklist.js
+++ components/linklist.js
@@ -2,24 +2,26 @@ import Link from "next/link";
 import React from "react";
 const LinkList = () => {
   return (
-    <>
+    <header>
       <ul>
         <li>
-          <Link as="/post/papayaCookie" href="/post/[id]">
+          <Link as="/post/1" href="/post/[id]">
             <a>Papaya Cookie</a>
           </Link>
         </li>
         <li>
-          <Link as="/post/dawnDusk" href="/post/[id]">
+          <Link as="/post/2" href="/post/[id]">
             {/* We use the as attribute to distinguish betwen the first / second post */}
             <a>Dawn and Dusk</a>
           </Link>
         </li>
       </ul>
       <Link href="../">
-        <a>Go back to the Previous Page</a>
+        <a>
+          <h3>Go back to the Previous Page</h3>
+        </a>
       </Link>
-    </>
+    </header>
   );
 };
 export default LinkList;
--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
+// import { useRouter } from "next/router";
+// import Link from "next/link";
 import LinkList from "../../../components/linklist";
 
-const Post = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  return (
-    <>
-      <h1>Page: {id}</h1>
-      <LinkList />
-    </>
+const Post = ({ title, body }) => (
+  // return (
+  // const router = useRouter();
+  // const { id } = router.query;
+
+  <>
+    <LinkList />
+    <h1>{title}</h1>
+    <p>{body}</p>
+  </>
+);
+
+Post.getInitialProps = async ({ query }) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${query.id}`
   );
+  const post = res.json();
+  return post;
 };
 export default Post;
